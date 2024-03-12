fd818d40c772199e42e37f16fc478d0e86461f28
Shreya Nigam <shreya.nigam1062@gmail.com>
2024-03-12 23:09:05 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,33 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./DB/connectDB.js";
-import {app, server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB(process.env.MONGO_URI);
